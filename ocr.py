b4e3bc71f912dc388c17b788f69b4150c3662ce7
Ali Najafi <mail.ali.najafi@gmail.com>
2017-07-02 16:49:54 +0430

    class api wrapper

---
+++ ocr.py
@@ -0,0 +1,71 @@
+import requests
+
+
+class OCRSpace:
+    def __init__(self, api_key, language=OCRSpaceLanguage.English):
+        """ ocr.space API wrapper
+        :param api_key: API key string
+        :param language: document language
+        """
+        self.api_key = api_key
+        self.language = language
+        self.payload = {
+            'isOverlayRequired': True,
+            'apikey': self.api_key,
+            'language': self.language,
+        }
+
+    def ocr_file(self, filename):
+        """ OCR.space API request with local file
+        :param filename: Your file path & name
+        :return: Result in JSON format
+        """
+        with open(filename, 'rb') as f:
+            r = requests.post(
+                'https://api.ocr.space/parse/image',
+                files={filename: f},
+                data=self.payload,
+            )
+        return r.content.decode()
+
+    def ocr_url(self, url):
+        """ OCR.space API request with remote file
+        :param url: Image url
+        :return: Result in JSON format.
+        """
+        r = requests.post(
+            'https://api.ocr.space/parse/image',
+            data=self.payload,
+        )
+        return r.content.decode()
+
+
+class OCRSpaceLanguage:
+    Arabic = 'ara'
+    Bulgarian = 'bul'
+    Chinese_Simplified = 'chs'
+    Chinese_Traditional = 'cht'
+    Croatian = 'hrv'
+    Danish = 'dan'
+    Dutch = 'dut'
+    English = 'eng'
+    Finnish = 'fin'
+    French = 'fre'
+    German = 'ger'
+    Greek = 'gre'
+    Hungarian = 'hun'
+    Korean = 'kor'
+    Italian = 'ita'
+    Japanese = 'jpn'
+    Norwegian = 'nor'
+    Polish = 'pol'
+    Portuguese = 'por'
+    Russian = 'rus'
+    Slovenian = 'slv'
+    Spanish = 'spa'
+    Swedish = 'swe'
+    Turkish = 'tur'
+
+
+if __name__ == '__main__':
+    ocr
