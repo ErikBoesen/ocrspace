89b59ded6a3d735c74ba0212399c61bdbb70db6e
ErikBoesen <me@erikboesen.com>
2019-04-06 11:48:46 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'ocr_space',
+    packages = ['ocr_space'], # this must be the same as the name above
+    version = '1.0',
+    description = 'Perform OCR through ocr.space API',
+    author = 'Erik Boesen',
+    author_email = 'me@erikboesen.com',
+    url = 'https://github.com/ErikBoesen/ocr_space',
+    keywords = ['ocr'],
+    classifiers = [],
+)
