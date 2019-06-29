3783db545b5a4767b71ce36151c67b99a57a4ca1
ErikBoesen <me@erikboesen.com>
2019-06-29 12:31:08 -0400

    Rename to ocrspace

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = 'ocr_space',
-    packages = ['ocr_space'], # this must be the same as the name above
-    version = '1.0',
+    name = 'ocrspace',
+    packages = ['ocrspace'], # this must be the same as the name above
+    version = '2.0',
     description = 'Perform OCR through ocr.space API',
     author = ['Ali Najafi', 'Erik Boesen'],
     author_email = ['mail.ali.najafi@gmail.com', 'me@erikboesen.com'],
-    url = 'https://github.com/a4fr/ocr_space_api_wrapper',
+    url = 'https://github.com/ErikBoesen/ocrspace',
     keywords = ['ocr'],
     classifiers = [],
 )
