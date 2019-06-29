c48f989acc485d6d9141fea26e8d8d02d7f52531
ErikBoesen <me@erikboesen.com>
2019-06-29 12:54:34 -0400

    Use simpler no-text link

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 import ocrspace
 
 api = ocrspace.API()
-from pprint import pprint
-pprint(api.ocr_url('https://images-na.ssl-images-amazon.com/images/I/71ovNJN1URL._SL1244_.jpg'))
-pprint(api.ocr_url('https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG/220px-Batian_Nelion_and_pt_Slade_in_the_foreground_Mt_Kenya.JPG'))
+print(api.ocr_url('https://images-na.ssl-images-amazon.com/images/I/71ovNJN1URL._SL1244_.jpg'))
+print(api.ocr_url('https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Solid_blue.svg/225px-Solid_blue.svg.png'))
