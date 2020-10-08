faf2e2bc2b4cbfc8b3e3ed131785309dedc254ca
ErikBoesen <me@erikboesen.com>
2020-10-07 23:14:21 -0600

    Bump to 2.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'ocrspace',
     packages = ['ocrspace'], # this must be the same as the name above
-    version = '2.2.0',
+    version = '2.2.1',
     description = 'Perform OCR through ocr.space API',
     author = ['Ali Najafi', 'Erik Boesen'],
     author_email = 'me@erikboesen.com',
