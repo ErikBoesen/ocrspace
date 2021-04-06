9c280e3daa899228b8e7d9cf38da0fc2887863ac
ErikBoesen <me@erikboesen.com>
2021-04-05 20:12:21 -0400

    Demonstrate ocr_file in example.py

--- example.py
+++ example.py
@@ -1,5 +1,22 @@
 import ocrspace
+import requests
+
 
 api = ocrspace.API()
-print(api.ocr_url('https://images-na.ssl-images-amazon.com/images/I/71ovNJN1URL._SL1244_.jpg'))
-print(api.ocr_url('https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Solid_blue.svg/225px-Solid_blue.svg.png'))
+TEST_IMAGE_URL = 'https://images-na.ssl-images-amazon.com/images/I/71ovNJN1URL._SL1244_.jpg'
+
+print('Testing URL-based OCR:')
+print(api.ocr_url(TEST_IMAGE_URL))
+
+print('Testing file-based OCR:')
+# Download image for demo purposes
+TEST_FILENAME = '/tmp/test_image.jpg'
+with open(TEST_FILENAME, 'wb') as f:
+    r = requests.get(TEST_IMAGE_URL)
+    r.raw.decode_content = True
+    f.write(r.content)
+
+# With file path
+print(api.ocr_file(TEST_FILENAME))
+# With file pointer
+print(api.ocr_file(open(TEST_FILENAME, 'rb')))
